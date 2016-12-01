c178a73d09ce282243793c0b92f561608936a761
raghavnyati90 <raghavnyati90@gmail.com>
2016-11-30 16:49:39 -0800

    Initial Commit

---
+++ myFbLoginTests/myFbLoginTests.swift
@@ -0,0 +1,36 @@
+//
+//  myFbLoginTests.swift
+//  myFbLoginTests
+//
+//  Created by Raghav Nyati on 11/30/16.
+//  Copyright © 2016 Raghav Nyati. All rights reserved.
+//
+
+import XCTest
+@testable import myFbLogin
+
+class myFbLoginTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
